b770fd5c2cc4765792061a5689322bc096e68d04
Fidian Peralta Lopez <donyoh1@hotmail.com>
2026-03-19 19:08:17 -0500

    Agregando una imagenes, arreglando css y html

--- css/js/menu.js
+++ css/js/menu.js
@@ -1,8 +1,22 @@
-const menuBtn = document.getElementById("menu-btn");
-const navMenu = document.getElementById("nav-menu");
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.getElementById("menu-btn");
+    const navMenu = document.getElementById("nav-menu");
 
-menuBtn.addEventListener("click", () => {
-
-navMenu.classList.toggle("active");
-
+    if (menuBtn && navMenu) {
+        menuBtn.addEventListener("click", () => {
+            navMenu.classList.toggle("active");
+            menuBtn.classList.toggle("open");
+        });
+    }
 });
+
+/*document.querySelector('.eye').addEventListener('click', function() {
+    const input = document.getElementById('password');
+    if (input.type === 'password') {
+        input.type = 'text';
+        this.querySelector('span').textContent = 'visibility_off';
+    } else {
+        input.type = 'password';
+        this.querySelector('span').textContent = 'visibility';
+    }
+});*/
